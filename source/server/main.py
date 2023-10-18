346647c94fc625a7c9ca72bec6d0be85d172c88a
NotHydra <rizky.irswanda115@gmail.com>
2023-10-18 22:42:48 +0800

    feat: user update level check

--- source/server/main.py
+++ source/server/main.py
@@ -197,44 +197,56 @@ def userCreate(response: Response, body: UserModel):
 @app.put("/api/user/update/{id}")
 def userUpdate(response: Response, id: int, body: UserUpdateModel):
     try:
-        user = database.getCollection("user")
-        documentObject = user.find_one({"_id": id})
-
-        if documentObject:
-            documentObject = user.update_one(
-                {"_id": id},
-                {
-                    "$set": {
-                        "name": body.name,
-                        "username": body.username,
-                        "email": body.email,
-                        "level": body.level,
-                        "updatedAt": datetime.datetime.now(),
-                    }
-                },
-            )
+        if body.level in ["user", "admin"]:
+            user = database.getCollection("user")
+            documentObject = user.find_one({"_id": id})
 
             if documentObject:
-                response.status_code = status.HTTP_202_ACCEPTED
-
-                return Utility.formatResponse(
-                    True,
-                    response.status_code,
-                    "User Updated",
-                    user.find_one({"_id": id}),
+                documentObject = user.update_one(
+                    {"_id": id},
+                    {
+                        "$set": {
+                            "name": body.name,
+                            "username": body.username,
+                            "email": body.email,
+                            "level": body.level,
+                            "updatedAt": datetime.datetime.now(),
+                        }
+                    },
                 )
+
+                if documentObject:
+                    response.status_code = status.HTTP_202_ACCEPTED
+
+                    return Utility.formatResponse(
+                        True,
+                        response.status_code,
+                        "User Updated",
+                        user.find_one({"_id": id}),
+                    )
+
+                else:
+                    response.status_code = status.HTTP_400_BAD_REQUEST
+
+                    return Utility.formatResponse(
+                        False, response.status_code, "User Failed To Be Updated", None
+                    )
 
             else:
-                response.status_code = status.HTTP_400_BAD_REQUEST
+                response.status_code = status.HTTP_404_NOT_FOUND
 
                 return Utility.formatResponse(
-                    False, response.status_code, "User Failed To Be Updated", None
+                    False, response.status_code, "User Not Found", None
                 )
+
         else:
-            response.status_code = status.HTTP_404_NOT_FOUND
+            response.status_code = status.HTTP_400_BAD_REQUEST
 
             return Utility.formatResponse(
-                False, response.status_code, "User Not Found", None
+                False,
+                response.status_code,
+                "User Role Needs To Be User or Admin",
+                None,
             )
 
     except Exception as e:
