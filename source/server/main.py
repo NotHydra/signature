5f81d7b6a4583f9e1979fc993b35d585d7e7f6fe
NotHydra <rizky.irswanda115@gmail.com>
2023-11-11 11:36:45 +0800

    feat: get access count

--- source/server/main.py
+++ source/server/main.py
@@ -854,6 +854,28 @@ def user(response: Response, body: AccessPageModel):
         return Utility.formatResponse(False, response.status_code, "Server Error", None)
 
 
+@app.get("/api/access/count")
+def userCount(response: Response):
+    try:
+        response.status_code = status.HTTP_200_OK
+
+        access = database.getCollection("access")
+        return Utility.formatResponse(
+            True,
+            response.status_code,
+            "Access Count",
+            {
+                "total": access.count_documents({}),
+            },
+        )
+
+    except Exception as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+        print(str(e))
+        return Utility.formatResponse(False, response.status_code, "Server Error", None)
+
+
 if __name__ == "__main__":
     import uvicorn
 
