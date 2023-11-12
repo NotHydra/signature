24eb4a2eca60dbdec873ba70af8f1e65ad9e32ae
NotHydra <rizky.irswanda115@gmail.com>
2023-11-12 18:59:51 +0800

    feat: implement document remove

--- source/client/app.py
+++ source/client/app.py
@@ -2323,6 +2323,11 @@ class App(ctk.CTk):
             Call.resetFrameCall()
             Middleware.refreshSessionDataMiddleware(self.documentAccessFrame, 1, id)
 
+        def removeButtonEvent(id: int) -> None:
+            Call.resetFrameCall()
+            Middleware.refreshSessionDataMiddleware(self.documentRemoveFrame, id)
+
+
         self.sidebarId = 2
 
         self.rowconfigure(0, weight=1)
@@ -2510,7 +2515,7 @@ class App(ctk.CTk):
                         "icon": "remove",
                         "mainColor": Dependency.colorPalette["danger"],
                         "hoverColor": Dependency.colorPalette["danger-dark"],
-                        "event": lambda: None,
+                        "event": removeButtonEvent,
                         "optional": True,
                     },
                 ],
@@ -3237,7 +3242,147 @@ class App(ctk.CTk):
             )
 
     def documentRemoveFrame(self, id: int) -> None:
-        pass
+        def removeButtonEvent() -> None:
+            if Message.confirmationMessage():
+                response = None
+
+                try:
+                    response = requests.delete(
+                        f"http://localhost:8000/api/document/remove/{id}",
+                    ).json()
+
+                except requests.ConnectionError:
+                    Message.errorMessage("Make Sure You Are Connected To The Internet")
+
+                except:
+                    Message.errorMessage("Server Error")
+
+                if response != None:
+                    if response["success"] == True:
+                        Message.successMessage(response["message"])
+
+                        Call.resetFrameCall()
+                        Middleware.refreshSessionDataMiddleware(self.documentFrame)
+
+                    else:
+                        Message.errorMessage(response["message"])
+
+                        if response["status"] == 404:
+                            Call.logoutCall()
+
+        def backButtonEvent() -> None:
+            Call.resetFrameCall()
+            Middleware.refreshSessionDataMiddleware(self.documentFrame)
+
+        response = None
+        try:
+            response = requests.get(f"http://localhost:8000/api/document/{id}").json()
+
+        except:
+            Message.errorMessage("Server Error")
+
+            backButtonEvent()
+
+        if response != None and response["success"] == False:
+            Message.errorMessage(response["message"])
+
+            backButtonEvent()
+
+        else:
+            self.sidebarId = 2
+
+            self.rowconfigure(0, weight=1)
+            self.columnconfigure(0, weight=1)
+            self.columnconfigure(1, weight=31)
+
+            Component.sidebarComponent()
+
+            contentFrame = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
+            contentFrame.rowconfigure(1, weight=1)
+            contentFrame.columnconfigure(0, weight=1)
+            contentFrame.grid(row=0, column=1, padx=20, sticky="nsew")
+
+            Component.titleContentComponent(contentFrame, title="DOCUMENT", row=0)
+
+            containerContentFrame = ctk.CTkFrame(
+                contentFrame,
+                corner_radius=8,
+                fg_color=Dependency.colorPalette["main"],
+            )
+            containerContentFrame.columnconfigure(0, weight=1)
+            containerContentFrame.grid(row=1, column=0, pady=(0, 20), sticky="nsew")
+
+            Component.titleContainerComponent(
+                containerContentFrame, title="Remove Document", row=0
+            )
+            Component.lineHorizontalComponent(containerContentFrame, row=1)
+
+            dataContainerFrame = ctk.CTkFrame(
+                containerContentFrame,
+                corner_radius=0,
+                fg_color="transparent",
+            )
+            dataContainerFrame.columnconfigure([0, 1], weight=1)
+            dataContainerFrame.grid(
+                row=2, column=0, padx=10, pady=(5, 0), sticky="nsew"
+            )
+
+            Component.entryDataComponent(
+                dataContainerFrame,
+                title="Code",
+                placeholder="code",
+                value=response["data"]["code"],
+                state=False,
+                row=0,
+                column=0,
+            )
+            Component.entryDataComponent(
+                dataContainerFrame,
+                title="Title",
+                placeholder="title",
+                value=response["data"]["title"],
+                state=False,
+                row=0,
+                column=1,
+            )
+
+            Component.entryDataComponent(
+                dataContainerFrame,
+                title="Category",
+                placeholder="category",
+                value=response["data"]["category"],
+                state=False,
+                row=1,
+                column=0,
+            )
+            Component.entryDataComponent(
+                dataContainerFrame,
+                title="Description",
+                placeholder="description",
+                value=response["data"]["description"],
+                state=False,
+                row=1,
+                column=1,
+            )
+
+            Component.buttonDataComponent(
+                dataContainerFrame,
+                text="Remove",
+                icon="remove",
+                mainColor=Dependency.colorPalette["danger"],
+                hoverColor=Dependency.colorPalette["danger-dark"],
+                event=removeButtonEvent,
+                row=2,
+            )
+            Component.buttonDataComponent(
+                dataContainerFrame,
+                text="Back",
+                icon="back",
+                mainColor=Dependency.colorPalette["danger"],
+                hoverColor=Dependency.colorPalette["danger-dark"],
+                event=backButtonEvent,
+                row=3,
+            )
 
 
 if __name__ == "__main__":
