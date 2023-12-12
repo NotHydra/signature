439fa6e940fa20d7fc01b487ecc5618c8d834017
NotHydra <rizky.irswanda115@gmail.com>
2023-12-12 15:54:44 +0800

    feat: connect bulma

--- source/react/source/App.tsx
+++ source/react/source/App.tsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <h1>Hello World!</h1>
-  )
+    <div className="App">
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Hello Bulma!</h1>
+          <p className="subtitle">My React App with Bulma</p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App
