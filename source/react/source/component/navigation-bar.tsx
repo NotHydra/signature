728776ce42bfa43cc12db6054326424c632675bd
NotHydra <rizky.irswanda115@gmail.com>
2023-12-12 17:10:04 +0800

    feat: navbar icon

--- source/react/source/component/navigation-bar.tsx
+++ source/react/source/component/navigation-bar.tsx
@@ -1,24 +1,27 @@
+import { useState } from "react";
+
 import logo from "./../public/image/logo-original.png";
 
 
 export const NavigationBar = () => {
+    const [toggle, setToggle] = useState(false)
+
     return (
-        <nav className="navbar" role="navigation" aria-label="main navigation">
+        <nav className="navbar is-fixed-top has-shadow" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <a className="navbar-item" href="/document">
                     <img className="mr-2" src={logo}></img>
-
                     <h1 className="subtitle has-text-info">Signature</h1>
                 </a>
 
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a role="button" className={`navbar-burger ${toggle ? "is-active" : ""}`} onClick={() => { setToggle(!toggle) }}>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
             </div>
 
-            <div id="navbarBasicExample" className="navbar-menu">
+            <div className={`navbar-menu ${toggle ? "is-active" : ""}`}>
                 <div className="navbar-start">
                     <a className="navbar-item" href="/document">
                         <h1 className="subtitle">
@@ -30,7 +33,11 @@ export const NavigationBar = () => {
                 <div className="navbar-end">
                     <a className="navbar-item" href="/profile">
                         <h1 className="subtitle">
-                            Profile
+                            <span className="icon mr-1 is-medium">
+                                <i className="fas fa-lg fa-user-circle"></i>
+                            </span>
+
+                            <span>Profile</span>
                         </h1>
                     </a>
                 </div>
