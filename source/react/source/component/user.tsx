9018fb9eb8bb5089254effd0492fbdbfff32e9b0
NotHydra <rizky.irswanda115@gmail.com>
2023-12-13 12:02:55 +0800

    feat: user component

---
+++ source/react/source/component/user.tsx
@@ -0,0 +1,29 @@
+import IUser from "../interface/user";
+
+export const CUser: React.FC<{
+    user: IUser;
+}> = ({ user }) => {
+    return (
+        <div className="column is-one-quarter">
+            <div className="card">
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-content">
+                            <p className="title is-4">{user.name}</p>
+
+                            <p className="subtitle is-6 mb-2">
+                                {`${user.username} - ${user.role}`}
+                            </p>
+
+                            <p className="subtitle is-7">{user.email}</p>
+                        </div>
+                    </div>
+
+                    <div className="content">
+                        {new Date(user.created_at).toLocaleDateString("en-GB")}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
