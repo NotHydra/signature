842c696f96232ab59af7a8151459342032b3f266
NotHydra <rizky.irswanda115@gmail.com>
2023-12-13 12:25:44 +0800

    feat: fetch total

--- source/react/source/page/document.tsx
+++ source/react/source/page/document.tsx
@@ -1,43 +1,63 @@
 import axios from "axios";
 import { ReactElement, useEffect, useState } from "react";
 
-import IFormatResponse from "../interface/format-response";
-import IDocument from "../interface/document";
+import { IFormatResponse } from "../interface/format-response";
+import { IDocument, IDocumentTotal } from "../interface/document";
 
 import { CDocuments } from "../component/documents";
 
 export const Document = (): ReactElement => {
     const [documents, setDocuments] = useState<IDocument[]>([]);
+    const [total, setTotal] = useState<number>(0);
+
+    const fetchDocument = async () => {
+        const response = await axios<IFormatResponse<IDocument[]>>({
+            method: "get",
+            url: "https://signature-api.irswanda.com/api/document",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            params: {
+                count: 12,
+                page: 1,
+            },
+        });
+
+        if (response.data.success) {
+            setDocuments(response.data.data);
+        } else {
+            setDocuments([]);
+        }
+    };
+
+    const fetchTotal = async () => {
+        const response = await axios<IFormatResponse<IDocumentTotal>>({
+            method: "get",
+            url: "https://signature-api.irswanda.com/api/document/count",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.data.success) {
+            setTotal(response.data.data.total);
+        } else {
+            setTotal(0);
+        }
+    };
 
     useEffect(() => {
-        const fetchDocument = async () => {
-            const response = await axios<IFormatResponse<IDocument[]>>({
-                method: "get",
-                url: "https://signature-api.irswanda.com/api/document",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                params: {
-                    count: 12,
-                    page: 1,
-                },
-            });
-
-            if (response.data.success) {
-                setDocuments(response.data.data);
-            } else {
-                setDocuments([]);
-            }
-        };
-
         fetchDocument();
+        fetchTotal();
     }, []);
 
     return (
         <section className="section">
             <div className="container">
                 <h1 className="title">Document</h1>
-                <p className="subtitle">{documents.length} Total Documents</p>
+                <p className="subtitle">
+                    {documents.length} out of {total} total documents
+                </p>
 
                 {documents.length > 0 ? (
                     <CDocuments documents={documents} />
