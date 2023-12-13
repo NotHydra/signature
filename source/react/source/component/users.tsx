7df097e706ab31b265e6ed53f65f1c899ac2f300
NotHydra <rizky.irswanda115@gmail.com>
2023-12-13 14:02:35 +0800

    feat: remove is centered from user

--- source/react/source/component/users.tsx
+++ source/react/source/component/users.tsx
@@ -6,7 +6,7 @@ export const CUsers: React.FC<{
     users: IUser[];
 }> = ({ users }) => {
     return (
-        <div className="columns is-multiline is-centered">
+        <div className="columns is-multiline">
             {users.map((user: IUser) => (
                 <CUser key={user._id} user={user} />
             ))}
